dd3145f3b86a7fc185a3adc1d6818be86eadcca1
tanvi-jangda <tanvi.jangda@gmail.com>
2023-05-09 18:14:02 +0530

    Implemented repository interface and repository class in data access folder

---
+++ eBookSite.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookSite.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T:class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ eBookSite.DataAccess/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using eBookSite.DataAccess.Data;
+using eBookSite.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBookSite.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _dbContext;
+        internal DbSet<T> _dbSet;
+        public Repository(ApplicationDBContext db)
+        {
+            this._dbContext = db;
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+            
+        }
+
+        public T GetById(Expression<Func<T, bool>> filter)
+        {
+           IQueryable<T> query = _dbSet.Where(filter);
+            return query.FirstOrDefault(); ;
+        }
+
+        public void Remove(T entity)
+        {
+          _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+           _dbSet.RemoveRange(entities);
+        }
+    }
+}
